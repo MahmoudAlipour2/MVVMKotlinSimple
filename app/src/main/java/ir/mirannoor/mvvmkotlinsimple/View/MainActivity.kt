742a96c1a5383b226692ecb9bfa6938401c9b1fb
unknown <ostovaar@gmail.com>
2019-06-11 13:27:36 +0430

    MVVM KOTLIN SIMPLE

--- app/src/main/java/ir/mirannoor/mvvmkotlinsimple/View/MainActivity.kt
+++ app/src/main/java/ir/mirannoor/mvvmkotlinsimple/View/MainActivity.kt
@@ -2,12 +2,35 @@ package ir.mirannoor.mvvmkotlinsimple.View
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import ir.mirannoor.mvvmkotlinsimple.R
+import ir.mirannoor.mvvmkotlinsimple.ViewModel.ViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    //Define viewModel
+    lateinit var viewModel: ViewModel
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        //Instance of ViewModel
+        viewModel = ViewModelProviders.of(this).get(ViewModel::class.java)
+
+        //Get data from ViewModel by LiveData
+        viewModel.observable.observe(this, Observer { Toast.makeText(this@MainActivity, it, Toast.LENGTH_LONG).show() })
+
+        btn.setOnClickListener {
+            //Send data from View to ViewModel.
+            viewModel.getUser()
+        }
+
+
     }
 }
--- app/src/main/java/ir/mirannoor/mvvmkotlinsimple/ViewModel/ViewModel.kt
+++ app/src/main/java/ir/mirannoor/mvvmkotlinsimple/ViewModel/ViewModel.kt
@@ -1,5 +1,24 @@
 package ir.mirannoor.mvvmkotlinsimple.ViewModel
 
-class ViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+
+//ViewModel---> androidx.lifecycle
+class ViewModel : ViewModel() {
+
+
+    //Define LiveData in ViewModel.
+    val observable = MutableLiveData<String>()
+
+
+
+
+    fun getUser() {
+
+        //Send data from "ViewModel to View"
+        observable.value = "Mahmoud"
+    }
+
 
 }
